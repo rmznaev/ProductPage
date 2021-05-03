91df5103aba7840403dcd10203f9b7af7268c570
Ramazan Abdullayev <ramazana@bankrespublika.az>
2021-05-03 11:42:53 +0400

    first commit

---
+++ ProductPage/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ProductPage
+//
+//  Created by Ramazan Abdullayev on 03.05.21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
